3545738583d135f341313060451bb21979f3d31e
Alicia McNett <amcnettpct@gmail.com>
2019-09-25 14:29:17 -0400

    Adding interrupt examples

---
+++ Examples/src/interrupts/ThreadInterruptExample2.java
@@ -0,0 +1,29 @@
+package interrupts;
+
+public class ThreadInterruptExample2 implements Runnable {
+
+	public static void main(String[] args) {
+        Thread t1 = new Thread(new ThreadInterruptExample2());
+        t1.start();
+ 
+        try {
+            Thread.sleep(5000);
+            t1.interrupt();
+ 
+        } catch (InterruptedException ex) {
+            // do nothing
+        }
+ 
+    }
+	
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("This is message #" + i);
+ 
+            if (Thread.interrupted()) {
+                System.out.println("I'm about to stop");
+                return;
+            }
+        }
+    } 
+}
